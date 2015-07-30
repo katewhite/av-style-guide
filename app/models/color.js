266393d5113f0197e19b7b5a382c8a0bb33bb85b
Kyle Coberly <kyle.coberly@gmail.com>
2015-07-30 12:48:04 -0600

    Fixing dependency errors and fixtures

--- app/models/color.js
+++ app/models/color.js
@@ -9,46 +9,57 @@ var Color = DS.Model.extend({
 Color.reopenClass({
   	FIXTURES: [
   	{
+        id: 1,
 	    name: '$blue-text',
 	    hexCode: '#59b4bf'
 	},
 	{
+        id: 2,
 		name: '$blue-app-panel',
 		hexCode: '#417380'
 	},
 	{
+        id: 3,
 		name: '$blue-symapp-panel',
 		hexCode: '#7fa1a9'
 	},
 	{
+        id: 4,
 		name: '$black-text',
 		hexCode: '#333333'
 	},
 	{
+        id: 5,
 		name: '$gray-toolbar',
 		hexCode: '#666666'
 	},
 	{
+        id: 6,
 		name: '$gray-inactive-text',
 		hexCode: '#b7b7b7'
 	},
 	{
+        id: 7,
 		name: '$gray-inactive-bg',
 		hexCode: '#e0e0e0'
 	},
 	{
+        id: 8,
 		name: '$white-container-bg',
 		hexCode: '#f5f5f5'
 	},
 	{
+        id: 9,
 		name: '$blue-light-bg',
 		hexCode: '#dde4e6'
 	},
 	{
+        id: 10,
 		name: '$red',
 		hexCode: '#cd2c24'
 	},
 	{
+        id: 11,
 		name: '$green',
 		hexCode: '#819964'
 	}
--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('colors');
+  this.route('color');
 });
 
 export default Router;
