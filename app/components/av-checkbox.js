a48a687f5785c6643cf0c6584afc4ac08db9bcd9
Kate White <white.kate.e@gmail.com>
2015-08-10 13:42:14 -0600

    Changed all bind-attr to use new handlebars helpers.  Modified getUniqueId to just a uniqueId property so that we are not setting a property value in didInsertElement

--- app/components/av-checkbox.js
+++ app/components/av-checkbox.js
@@ -3,13 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	tagName: "div",
 	classNames: ["av-checkbox"],
-	generateUniqueId: Ember.on("didInsertElement", function(){
-	   let s4 = function() {
-	    return Math.floor((1 + Math.random()) * 0x10000)
-	      .toString(16)
-	      .substring(1);
-		};
-		this.set("uniqueId", s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-			s4() + '-' + s4() + s4() + s4());
-	})
+	uniqueId: function() {
+	    let s4 = function() {
+			return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+ 		};
+		return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+	}.property()
 });
